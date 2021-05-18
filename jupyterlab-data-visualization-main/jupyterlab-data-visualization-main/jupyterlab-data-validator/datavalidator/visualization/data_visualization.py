ca66c9ea0a23a6014380cb26d328563a9a0ca74a
Te-Fuir-ai <72773958+Te-Fuir-ai@users.noreply.github.com>
2021-05-18 18:54:16 +0800

    Update data_visualization.py

--- jupyterlab-data-visualization-main/jupyterlab-data-visualization-main/jupyterlab-data-validator/datavalidator/visualization/data_visualization.py
+++ jupyterlab-data-visualization-main/jupyterlab-data-visualization-main/jupyterlab-data-validator/datavalidator/visualization/data_visualization.py
@@ -1,8 +1,3 @@
-# Thisdataframes=Nonethon script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 from datavalidator.handlers.statistics_handler import StatisticsHandler
 from datavalidator.handlers.convert_handler import ConvertHandler
